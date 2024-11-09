3a21895e869cc7cb5c04978c37c6f9d2d0958379
Ataa Katranji <ataa.katranji@gmail.com>
2024-11-09 15:40:19 +0300

    webPush

--- app/components/Notifications.js
+++ app/components/Notifications.js
@@ -1,22 +1,45 @@
+"use client";
 import { useEffect } from 'react';
-import PushNotifications from '@pusher/push-notifications-web';
+PUBLICK_VAPID_KEY = BF_1HacDiNE-HX7VqeSwIjgfIgXSFW5ZYJuwnZ1ngRSy9kftD1QssgilOp7nV3WJVdxCgTJAxh_FeqIeALiPzhU
 
 const Notifications = () => {
     useEffect(() => {
-        // Ensure this code runs only on the client side
-        if (typeof window !== 'undefined') {
-            const beamsClient = new PushNotifications({
-                instanceId: 'YOUR_INSTANCE_ID'
-            });
+        if ('serviceWorker' in navigator && 'PushManager' in window) {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker registered:', registration);
+                    return registration.pushManager.getSubscription();
+                })
+                .then(async subscription => {
+                    if (!subscription) {
+                        const publicKey = `${process.eenv.PUBLICK_VAPID_KEY}`;
+                        const convertedVapidKey = urlBase64ToUint8Array(publicKey);
+                        const newSubscription = await registration.pushManager.subscribe({
+                            userVisibleOnly: true,
+                            applicationServerKey: convertedVapidKey
+                        });
 
-            beamsClient.start()
-                .then(() => beamsClient.addDeviceInterest('admin-notifications'))
-                .then(() => {
-                    console.log('Successfully registered for push notifications!');
+                        // Send the subscription to your server to save it
+                        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/Noti/save-subscription`, {
+                            method: 'POST',
+                            body: JSON.stringify(newSubscription),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        });
+                    }
                 })
                 .catch(console.error);
         }
     }, []);
+
+    // Utility function to convert VAPID key
+    function urlBase64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+        const rawData = window.atob(base64);
+        return new Uint8Array([...rawData].map(char => char.charCodeAt(0)));
+    }
 
     return null;
 };
