1faf81b606296096d60900254a1ea33f68990d4d
Ataa Katranji <ataa.katranji@gmail.com>
2024-11-17 16:53:16 +0300

    edit detials page

--- app/components/ExportPDF.js
+++ app/components/ExportPDF.js
@@ -68,7 +68,7 @@ const exportMonthlyReportPDF = (data) => {
     startY: doc.lastAutoTable.finalY + 10, // Start after the summary table
   });
 
-  doc.save(`${data.summry.monthName}_Attendance_Report_${data.summary.employeeName}.pdf`);
+  doc.save(`${data.summary.monthName}_Attendance_Report_${data.summary.employeeName}.pdf`);
   toast.info("Monthly report exported as PDF!");
 };
 
