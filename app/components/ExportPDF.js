b3c72dc8c1ca5c4bf43361d89a163baecd96f197
Ataa Katranji <ataa.katranji@gmail.com>
2024-12-02 11:42:00 +0300

    edit export pdf

--- app/components/ExportPDF.js
+++ app/components/ExportPDF.js
@@ -34,7 +34,8 @@ const exportMonthlyReportPDF = (data) => {
     const year = date.getFullYear();
     const month = String(date.getMonth() +1 ).padStart(2, '0'); // Ensure month is two-digit
     const day = String(date.getDate()).padStart(2, '0'); // Ensure day is two-digit
-
+    console.log("1",entry.date)
+    console.log("2",date)
     // Format options
     const shortOptions = { weekday: 'short' };
     const longOptions = { weekday: 'long' };
