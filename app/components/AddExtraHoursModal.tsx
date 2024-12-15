37411b006cbea1904e5b5ffa9ace69c0633386ec
Ataa Katranji <ataa.katranji@gmail.com>
2024-12-15 14:28:40 +0300

    add extra hours aand handle it

--- app/components/AddExtraHoursModal.tsx
+++ app/components/AddExtraHoursModal.tsx
@@ -11,7 +11,7 @@ import {
 import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import { useEffect, useState } from "react";
-import { METHODS } from "http";
+
 type AddExtraHoursModalProps = {
   isOpen: boolean;
   onClose: () => void;
