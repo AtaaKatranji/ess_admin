1e1a94f2552e10c4375e9835052072da42869db4
Ataa Katranji <ataa.katranji@gmail.com>
2025-04-05 11:34:29 +0300

    new

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -43,7 +43,7 @@ export default function AdminLogin() {
         }),
         credentials:'include'
       });
-
+      console.log("response",response)
       if (response.ok) {
         const data: AuthResponse = await response.json();
         
