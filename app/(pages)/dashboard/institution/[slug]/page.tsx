d262ced0a854fb05649a325c0c546a3fe6e405c6
Ataa Katranji <ataa.katranji@gmail.com>
2025-07-05 13:12:07 +0300

    edit strchure of employees

--- app/(pages)/dashboard/institution/[slug]/page.tsx
+++ app/(pages)/dashboard/institution/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import InstitutionDashboard from "@/app/components/InstitutionDashboard";
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const safeSlug = Array.isArray(slug) ? slug[0] : slug;
   return <InstitutionDashboard activeSection="overview" slug={safeSlug} />;
