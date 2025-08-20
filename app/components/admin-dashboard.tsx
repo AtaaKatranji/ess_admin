92fef23aafafa8f6162bee6ba81e68af00cd4e75
E N M A <88634119+AtaaKatranji@users.noreply.github.com>
2025-08-20 10:19:17 +0300

    edit admin-dashboard

--- app/components/admin-dashboard.tsx
+++ app/components/admin-dashboard.tsx
@@ -470,6 +470,7 @@ export function AdminDashboard() {
                       cardClickGuardRef.current = true; // فعّل الحارس
                       setSelectedInstitutionForManage(institution.id);
                       setManageOpen(true);
+                      console.log("institution.id by press button",institution.id)
                     }}
                   >
                     <Users className="h-4 w-4" />
