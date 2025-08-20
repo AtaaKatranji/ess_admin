5c9fe9a8975c71e737b1c0dc7ea4be68f5067ba9
E N M A <88634119+AtaaKatranji@users.noreply.github.com>
2025-08-20 10:47:47 +0300

    edit admin-dashboard

--- app/components/admin-dashboard.tsx
+++ app/components/admin-dashboard.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {  TooltipProvider,  } from "@/components/ui/tooltip";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import InstitutionCard from "@/app/components/InstitutionCard"
 import { fetchInstitutionsByAdmin } from "@/app/api/institutions/institutions"
