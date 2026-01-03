8f7548c6e9195a8ec0ba899935fa291a048580a9
E N M A <88634119+AtaaKatranji@users.noreply.github.com>
2026-01-03 13:35:56 +0300

    edit shift report export

--- app/components/pdfmake-rtl.d.ts
+++ app/components/pdfmake-rtl.d.ts
@@ -1,11 +1,11 @@
 declare module '@digicole/pdfmake-rtl/build/pdfmake' {
-  const pdfMake: any
+  const pdfMake: PDFMake
   export default pdfMake
   export = pdfMake
 }
 
 declare module '@digicole/pdfmake-rtl/build/vfs_fonts' {
-  const pdfFonts: any
+  const pdfFonts: PDFFonts
   export default pdfFonts
   export = pdfFonts
 }
