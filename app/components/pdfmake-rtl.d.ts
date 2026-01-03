f4d232898400976ba720c6b0de41f22b700e5cc9
E N M A <88634119+AtaaKatranji@users.noreply.github.com>
2026-01-03 13:20:39 +0300

    Refine PDF export functionality to further reduce row size

---
+++ app/components/pdfmake-rtl.d.ts
@@ -0,0 +1,12 @@
+declare module '@digicole/pdfmake-rtl/build/pdfmake' {
+  const pdfMake: any
+  export default pdfMake
+  export = pdfMake
+}
+
+declare module '@digicole/pdfmake-rtl/build/vfs_fonts' {
+  const pdfFonts: any
+  export default pdfFonts
+  export = pdfFonts
+}
+
